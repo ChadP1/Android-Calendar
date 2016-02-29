4e940506cd79a1de4c3f647ccc45dfb42f451eba
ChadP1 <unopoop@gmail.com>
2016-02-28 22:29:08 -0600

    all done

--- app/src/main/java/cis3334/simpletext/SecondActivity.java
+++ app/src/main/java/cis3334/simpletext/SecondActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends Activity {
-	
+
 	String eventNum;		// number of the event from mainActivity
 	TextView tvTitle, tvSum, tvLoc, tvDate;
 
@@ -22,36 +22,40 @@ public class SecondActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_second);
-		
+
 		tvTitle = (TextView) findViewById(R.id.txtTitle);
 		tvSum = (TextView) findViewById(R.id.txtSummary);
 		tvLoc = (TextView) findViewById(R.id.txtLocation);
 		tvDate = (TextView) findViewById(R.id.txtDate);
-		
+
+		// get the data passed from the main activity using extras
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-		    eventNum = extras.getString("eventNum");
+			// grab the event number as a string from the main activity using extras
+			eventNum = extras.getString("eventNum");
 		}
-		
+		//depending on the event number, display different information on this form
 		if (eventNum.equals("1")) {
-			tvTitle.setText("Red Baraat");
-			tvSum.setText("Red Baraat has established a reputation as one of the best live bands playing in the world. Led by dhol drummer Sunny Jain, the nine piece band melds the infectious North Indian rhythm Bhangra with a host of sounds, namely funk, go-go, Latin, and jazz.");
+
+			tvTitle.setText("An Evening With The Voice");
+			tvSum.setText("NBC’s The Voice’s Madi Davis and twins Andi & Alex Peot will perform in concert");
 			tvLoc.setText("Mitchell Auditorium");
-			tvDate.setText("FRIDAY, FEBRUARY 22, 2013");
+			tvDate.setText("SUNDAY, FEBRUARY 28, 2016");
 		}
 		if (eventNum.equals("2")) {
-			tvTitle.setText("Billy McLaughlin and His Eclectic String Quartet");
-			tvSum.setText("Billy McLaughlin is recognized internationally as a best-selling guitar virtuoso, composer and inspirational performer. In 2001, Billy was diagnosed with focal dystonia, an incurable neuromuscular disease that robbed him of the ability to play his own music.");
+			tvTitle.setText("Thistles & Shamrocks");
+			tvSum.setText("An Evening of Scottish & Irish Music and Dance sponsored by the Duluth Scottish Heritage Association");
 			tvLoc.setText("Mitchell Auditorium");
-			tvDate.setText("FRIDAY, March 8, 2013");
+			tvDate.setText("FRIDAY, March 4, 2016");
 		}
 		if (eventNum.equals("3")) {
-			tvTitle.setText("Battlefield Band");
-			tvSum.setText("A bedrock of talent for over four decades,Battlefield Band continue to lead the way for Scotland with their inspired fusion of ancient and modern traditional music and song.");
+			tvTitle.setText("Why Civil Resistance Works with Erica Chenoweth");
+			tvSum.setText("Erica Chenoweth, Ph.D., is an associate professor at the Josef Korbel School of International Studies at the University of Denver and an associate senior researcher at the Peace Research Institute of Oslo.");
 			tvLoc.setText("Mitchell Auditorium");
-			tvDate.setText("FRIDAY, March 22, 2013");
+			tvDate.setText("THURSDAY, MARCH 10, 2016");
+
 		}
-		
+
 	}
 
 	@Override
@@ -60,17 +64,19 @@ public class SecondActivity extends Activity {
 		getMenuInflater().inflate(R.menu.layout_second, menu);
 		return true;
 	}
-	
+
 	public void onClickSendText (View v) {
-		
+
 		if (eventNum.equals("1")) {
-		
+
 			Intent calIntent = new Intent(Intent.ACTION_INSERT);
 			calIntent.setType("vnd.android.cursor.item/event");
-			calIntent.putExtra(Events.TITLE, "Red Baraat");
+
+			calIntent.putExtra(Events.TITLE, "An Evening With the Voice");
+
 			calIntent.putExtra(Events.EVENT_LOCATION, "Mitchell Auditorium");
-			GregorianCalendar begDate = new GregorianCalendar(2015, 2, 22, 7, 30);
-			GregorianCalendar endDate = new GregorianCalendar(2015, 2, 22, 9, 30);
+			GregorianCalendar begDate = new GregorianCalendar(2016, 2, 28, 7, 30);
+			GregorianCalendar endDate = new GregorianCalendar(2016, 2, 28, 9, 30);
 			calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
 					begDate.getTimeInMillis());
 			calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
@@ -80,29 +86,37 @@ public class SecondActivity extends Activity {
 		if (eventNum.equals("2")) {
 			Intent calIntent = new Intent(Intent.ACTION_INSERT);
 			calIntent.setType("vnd.android.cursor.item/event");
-			calIntent.putExtra(Events.TITLE, "Billy McLaughlin");
+
+			calIntent.putExtra(Events.TITLE, "Thistles & Shamrocks");
+
 			calIntent.putExtra(Events.EVENT_LOCATION, "Mitchell Auditorium");
+			GregorianCalendar begDate = new GregorianCalendar(2016, 3, 4, 7, 30);
+			GregorianCalendar endDate = new GregorianCalendar(2016, 3, 4, 9, 30);
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+					begDate.getTimeInMillis());
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+					endDate.getTimeInMillis());
 			startActivity(calIntent);
 		}
 		if (eventNum.equals("3")) {
 			Intent calIntent = new Intent(Intent.ACTION_INSERT);
 			calIntent.setType("vnd.android.cursor.item/event");
-			calIntent.putExtra(Events.TITLE, "Battlefield Band");
+
+			calIntent.putExtra(Events.TITLE, "Why Civil Resistance Works with Erica Chenoweth");
+
 			calIntent.putExtra(Events.EVENT_LOCATION, "Mitchell Auditorium");
-			Calendar beginTime = Calendar.getInstance();
-			beginTime.set(2015, 2, 22, 7, 30);
-			Calendar endTime = Calendar.getInstance();
-			endTime.set(2015, 2, 22, 9, 30);
+			GregorianCalendar begDate = new GregorianCalendar(2016, 3, 10, 12, 30);
+			GregorianCalendar endDate = new GregorianCalendar(2016, 3, 10, 2, 30);
 			calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-					beginTime.getTimeInMillis());
+					begDate.getTimeInMillis());
 			calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-					endTime.getTimeInMillis());
+					endDate.getTimeInMillis());
 			startActivity(calIntent);
 
 		}
-		
+
 
 	}
-	
+
 
 }
